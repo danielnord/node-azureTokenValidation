c54689df17d15e8a0bbea3840f10909481b718a7
danielnord <dan@svep.se>
2020-11-04 17:43:03 +0100

    Change to b2clogin.com instead of public keys URL
    This will change validation of keys from login.microsoftonline.com to Tenant Azure B2C login.
    
    When using B2C URL instead of the public URL the data that is sent back is different:
    # FROM login.microsoftonline.com
    ```
    {
      "keys": [
        {
          "kty": "RSA",
          "use": "sig",
          "kid": "kg2LYs2T0CTjIfj4rt6JIynen38",
          "x5t": "kg2LYs2T0CTjIfj4rt6JIynen38",
          "n": "yTKa6m....",
          "x5c": [
            "MIIDBTCCAe2gAwIBAgIQQiR8gZNKuYpH6c......"
          ],
          "issuer": "https://login.microsoftonline.com/{tenantid}/v2.0"
        },
        {
          "kty": "RSA",
          "use": "sig",
          "kid": "18pnMg3UmrWvBK_tkDAbjgM5CmA",
          "x5t": "18pnMg3UmrWvBK_tkDAbjgM5CmA",
          "n": "v3tn90CV...",
          "e": "AQAB",
          "x5c": [
            "MIIC8TCCAdmgAwIBA..."
          ],
          "issuer": "https://login.microsoftonline.com/{tenantid}/v2.0"
        },
        ...
      ]
    }
    ```
    
    # To https://<tenant-name>.b2clogin.com/...
    From jwks_uri:
    ```
    {
      "keys": [
        {
          "kid": "X5eXk4xyojNFum1kl2Ytv8dlNP4-c57dO6QGTVBwaNk",
          "nbf": 1493763266,
          "use": "sig",
          "kty": "RSA",
          "e": "AQAB",
          "n": "tVKUtc..."
        }
      ]
    }
    ```
    
    
    
    
    Replace <tenant-name> with Tenant name. Example: mytenantname.b2clogin.com/mytenantname

--- azureTokenValidation.js
+++ azureTokenValidation.js
@@ -1,24 +1,26 @@
 // Run this by calling ```node tokenValidation.js``` in the console
 const jwt = require('jsonwebtoken')
 const fetch = require('node-fetch')
-var forge = require('node-forge')
+const getPem = require('rsa-pem-from-mod-exp')
 
 class AzureTokenValidation {
-    /**
+    constructor() {
+    }
+     /*
      * @param {String} idToken The Azure ID token to validate
      */
     async validate(idToken) {
         try {
             const kid = this.getKid(idToken)
             const publicKey = await this.getPublicKey(kid)
-
+​
             const t = jwt.verify(idToken, publicKey, { algorithms: ['RS256'] })
             return t
         } catch(exception) {
             return false
         }
     }
-
+​
     /**
      * @param {String} idToken The Azure ID token to get kid from
      */
@@ -33,20 +35,15 @@ class AzureTokenValidation {
     async getPublicKey(kid) {
         const signingKeys = await this.fetchSigningKeyInformation()
         const key = signingKeys.keys.find(k => k.kid === kid)
-        // console.log(key)
-        const msPublicKey = key.x5c[0]
-        const PEMSTART = "-----BEGIN CERTIFICATE-----\n"
-        const PEMEND = "\n-----END CERTIFICATE-----\n"
-        const pem = PEMSTART + msPublicKey + PEMEND
-        const certificate = forge.pki.certificateFromPem(pem)
-        return forge.pki.publicKeyToPem(certificate.publicKey)
+        const pem = getPem(key.n, key.e)
+        return pem
     }
-
+​
     /**
      * Fetch public signing key information from Microsoft
      */
     async fetchSigningKeyInformation() {
-        const urlKeys = 'https://login.microsoftonline.com/common/discovery/v2.0/keys'
+        const urlKeys = 'https://<tenant-name>.b2clogin.com/<tenant-name>.onmicrosoft.com/discovery/v2.0/keys?p=<user-flow>'
         const keys = await fetch(urlKeys, { method: "Get" })
         const jsonKeys = await keys.json()
         return jsonKeys
